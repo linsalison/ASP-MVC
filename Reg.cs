1840c0c3aff92078aadec8fe408b82fdbd9dd1a4
linsalison <37173808+linsalison@users.noreply.github.com>
2019-02-28 15:34:03 +0530

    Add files via upload

---
+++ Reg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace registermvc.Models
+{
+    public class Reg
+    {
+        [Required(ErrorMessage = "can't be empty")]
+        [Display(Name = "Student name")]
+        // [RegularExpression()]
+
+        public string Sname { get; set; }
+
+        [Required]
+        [Display(Name = "Student address")]
+        [DataType(DataType.MultilineText)]
+        public string Saddress { get; set; }
+        [Required]
+        [Display(Name ="select one")]
+      
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "select one")]
+        public Course SCourse { get; set; }
+        [Display(Name ="Upload Image")]
+        public string Image { get; set; }
+
+        public HttpPostedFileBase ImageFile { get; set; }
+
+        [Required]
+        [Display(Name = "Student email")]
+        [DataType(DataType.EmailAddress)]
+        public string Semail { get; set; }
+
+        [Required(ErrorMessage = "can't be empty")]
+        [Display(Name = "Student username")]
+        public string Uname { get; set; }
+
+        [Required(ErrorMessage = "can't be empty")]
+        [Display(Name = "Student Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "can't be empty")]
+        [Display(Name = "confirm password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+    public enum Course
+    {
+       MCA,
+       MBA,
+       MTECH
+    }
+}
